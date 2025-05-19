6d09a2d857039b0aa39af097d27daa0423e2a61b
f11m4at4 <f11m4at4@gmail.com>
2025-05-19 11:12:46 +0900

    프로젝트생성

---
+++ Source/NetTPS/Public/NetTPS.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(NetTPS, Log, All);
+
+// 호출하는 함수와 줄번호 정보
+#define APPINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define PRINT_APPINFO() UE_LOG(NetTPS, Warning, TEXT("%s"), *APPINFO)
+#define PRINTLOG(fmt,...) \
+	UE_LOG(NetTPS, Warning, TEXT("%s : %s"), *APPINFO, *FString::Printf(fmt, ##__VA_ARGS__))
